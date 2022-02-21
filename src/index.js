fb6276acb9a5a50fb5193729cb896199da29e42f
Svetik-K <sviatlana_kliautsova@icloud.com>
2022-02-21 22:12:40 +0100

    feat: add 'check' function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.join('').replace(/,/g, '');
+  let stack = [];
+  
+  for(let bracket of str) {
+    let bracketsIndex = config.indexOf(bracket);
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1);
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }  
+  }
+  return stack.length === 0;
 }
+
