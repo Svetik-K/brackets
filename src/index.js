0a9ca2c16abc6e23ddf7e7d930da677386662a42
Svetik-K <sviatlana_kliautsova@icloud.com>
2022-07-14 16:25:08 +0200

    fix: brackets

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  const pattern = bracketsConfig.join('').replace(/,/g, '');
   let stack = [];
-  
   for(let i = 0; i < str.length; i++) {
-    let indexInPattern = pattern.indexOf(str[i]);
-    if(indexInPattern % 2 === 0) {
-      stack.push(str[i]);
-    } else {
-      let lastStackItem = stack[stack.length - 1];
-      if(pattern.indexOf(lastStackItem) === (indexInPattern - 1)) {
-        stack.pop();
-      } else {
-        return false;
+    bracketsConfig.forEach(pair => {
+      if(str[i] === pair[0]) {
+        if(str[i] === pair[1]) {
+          if(stack.indexOf(str[i]) === -1) {
+            stack.push(str[i]);
+          } else {
+            stack.splice(stack.indexOf(str[i]), 1);
+          }
+        } else {
+          stack.push(str[i]);
+        }
+      } 
+      else if(str[i] === pair[1] && stack[stack.length - 1] === pair[0]) {
+          stack.pop();
       }
-    }  
+    })
   }
-
   return stack.length === 0 ? true : false;
+
 }
+
 
